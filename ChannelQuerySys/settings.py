0a816f9c554aa93320c7e17e8dac5963b231463d
Charles-ZZPro <batista19902011@gmail.com>
2017-09-27 19:38:08 +0800

    last

--- ChannelQuerySys/settings.py
+++ ChannelQuerySys/settings.py
@@ -91,25 +91,25 @@ WSGI_APPLICATION = 'ChannelQuerySys.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'myTestDB',
-        #'USER': 'postgres',
-        'USER': 'littleAdmin',
-        'PASSWORD': 'postgres',
-        #'HOST':'120.77.179.136',
-        'HOST':'127.0.0.1',
-        'PORT':'5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'myTestDB',
+#         #'USER': 'postgres',
+#         'USER': 'littleAdmin',
+#         'PASSWORD': 'postgres',
+#         #'HOST':'120.77.179.136',
+#         'HOST':'127.0.0.1',
+#         'PORT':'5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
